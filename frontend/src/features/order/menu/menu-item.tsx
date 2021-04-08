2ed3d8a34b06b3908ffede80c3db08ab4c69e211
Igor Tatarinov <igor.tanv@gmail.com>
2021-04-08 23:10:33 +0700

    cleared item count upon submit

--- frontend/src/features/order/menu/menu-item.tsx
+++ frontend/src/features/order/menu/menu-item.tsx
@@ -15,19 +15,22 @@ const FoodItemLocalStore = {
     localStorage.setItem(`menu_item_id_${id}`, String(count));
   },
   getCount(id: Number) {
-    return parseInt(localStorage.getItem(`menu_item_id_${id}`) || "0");
+    return parseInt(String(localStorage.getItem(`menu_item_id_${id}`))) || 0;
   },
 };
 
 export default function MenuItem({ onChange, item, type }: MenuItemProps) {
   const [data, setData] = useState({
-    count: FoodItemLocalStore.getCount(item.id),
+    count: FoodItemLocalStore.getCount(item.id), // default!!!
   });
 
+  data.count = FoodItemLocalStore.getCount(item.id);
+
   const menuItemCountChange = (e: any) => {
-    data.count = parseInt(e.target.value);
+    data.count = parseInt(e.target.value) || 0;
     FoodItemLocalStore.setCount(item.id, data.count);
     setData({ ...data });
+    //onChange(item.id, item.price, data.count, type);
   };
 
   onChange(item.id, item.price, data.count, type);
@@ -40,7 +43,7 @@ export default function MenuItem({ onChange, item, type }: MenuItemProps) {
           type="number"
           className="menu-item-count"
           min="0"
-          value={data.count}
+          value={String(data.count)}
           onChange={menuItemCountChange}
         />
         <strong className="item-price">${item.price.toFixed(2)}</strong>
